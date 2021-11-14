c73c80e69e169f09a3488dea844ffd6bfe5bc764
mt-igarashi <93467360+mt-igarashi@users.noreply.github.com>
2021-11-15 01:23:39 +0900

    子部品に関する検証

--- Helpers/EditContextHelper.cs
+++ Helpers/EditContextHelper.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Microsoft.AspNetCore.Components.Forms;
 
 namespace BlazorApp.Helpers
@@ -8,14 +9,52 @@ namespace BlazorApp.Helpers
     public static class EditContextHelper
     {
         /// <summary>
+        /// 検証実行後のスタイルを返却します。
+        /// </summary>
+        /// <param name="hasError">エラーフラグ</param>
+        /// <returns>スタイル</returns>
+        public static string GetFieldCss(bool hasError)
+        {
+            if (hasError)
+            {
+                return "invalid";
+            }
+            else
+            {
+                return "modified";
+            }
+        }
+
+        /// <summary>
         /// 指定したプロパティの変更イベントを通知します。
         /// </summary>
-        /// <param name="context"></param>
-        /// <param name="name"></param>
-        public static void NotifyFieldChanged(EditContext context, string name)
+        /// <param name="context">EditContext</param>
+        /// <param name="name">プロパティ名</param>
+        /// <return>エラーあり/なし</return>
+        public static bool NotifyFieldChanged(EditContext context, string name)
         {
             var field = context.Field(name);
             context.NotifyFieldChanged(field);
+
+            var info = PropertyHelper.GetPropertyInfo(context.Model, name);
+            var displayName = PropertyHelper.GetDisplayName(info);
+
+            return context.GetValidationMessages().Any(x => x.Contains(displayName));
+        }
+
+        /// <summary>
+        /// 指定したフィールドに検証エラーがあるかを検証します。
+        /// </summary>
+        /// <param name="context">EditContext</param>
+        /// <param name="name">プロパティ名</param>
+        /// <return>エラーあり/なし</return>
+        public static bool HasValidationError(EditContext context, string name) 
+        {
+            var field = context.Field(name);
+            var info = PropertyHelper.GetPropertyInfo(context.Model, name);
+            var displayName = PropertyHelper.GetDisplayName(info);
+
+            return context.GetValidationMessages().Any(x => x.Contains(displayName));
         }
     }
 }
